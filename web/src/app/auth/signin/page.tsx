714b80421e4a7605aa8c984ea50bb6c12bac7ef7
Rishabh Awatani <rishabh@nuevezo.com>
2023-08-04 08:56:35 -0400

    Authentication master layout added

--- web/src/app/auth/signin/page.tsx
+++ web/src/app/auth/signin/page.tsx
@@ -18,91 +18,62 @@ import { EnvelopeIcon } from '@heroicons/react/24/outline';
 import { useSearchParams } from 'next/navigation';
 import Button from '@/components/Button';
 import TextBox from '@/components/TextBox';
-export default function SignIn() {
+const SignIn = () => {
   const searchParams = useSearchParams();
   const callbackUrl = (searchParams.get('callbackUrl') as string) ?? '/';
-  const [isMobile] = useMediaQuery('(max-width: 768px)');
   return (
-    <Flex direction={'row'}>
-      <Box
-        width={{
-          xl: '38vw',
-          lg: '68vw',
-          md: '100vw',
-          sm: '100vw',
-          xs: '100vw',
-        }}
-      >
-        <Flex
-          alignItems={'start'}
-          justifyContent={'center'}
-          height={'100vh'}
-          mx={isMobile ? '8' : '52'}
-          direction={'column'}
-          rowGap={'8'}
+    <>
+      <Heading>Log In</Heading>
+      <Stack width={'100%'}>
+        <TextBox
+          type="email"
+          placeholder="Email"
+          leftIcon={<EnvelopeIcon height={'20'} />}
+        />
+        <TextBox type="password" />
+      </Stack>
+      <Stack width={'100%'}>
+        <Button
+          type={'outline'}
+          onClick={() => {
+            console.log('click');
+          }}
         >
-          <Heading>Log In</Heading>
-          <Stack width={'100%'}>
-            <TextBox
-              type="email"
-              placeholder="Email"
-              leftIcon={<EnvelopeIcon height={'20'} />}
-            />
-            <TextBox type="password" />
-          </Stack>
-          <Stack width={'100%'}>
-            <Button
-              type={'outline'}
-              onClick={() => {
-                console.log('click');
-              }}
-            >
-              Log In
-            </Button>
-            <Text align={'center'} fontWeight={'bold'}>
-              or
-            </Text>
-            <Button
-              leftIcon={<FontAwesomeIcon icon={faGithub} />}
-              type={'solid'}
-              onClick={() => {
-                signIn('github', { callbackUrl: callbackUrl });
-              }}
-            >
-              Sign In with Github
-            </Button>
-            <Button
-              leftIcon={<FontAwesomeIcon icon={faMicrosoft} />}
-              type={'solid'}
-              onClick={() => {
-                signIn('github', { callbackUrl: callbackUrl });
-              }}
-            >
-              Sign In with Microsoft
-            </Button>
-            <Button
-              leftIcon={<FontAwesomeIcon icon={faGoogle} />}
-              type={'solid'}
-              onClick={() => {
-                signIn('github', { callbackUrl: callbackUrl });
-              }}
-            >
-              Sign In with Google
-            </Button>
-          </Stack>
-        </Flex>
-      </Box>
-      <Box
-        bgImage="url('/images/login-signup.jpg')"
-        bgPosition="center"
-        bgSize={'cover'}
-        bgRepeat="no-repeat"
-        width={'65vw'}
-        height={'100vh'}
-        display={isMobile ? 'none' : 'block'}
-      >
-        <Box></Box>
-      </Box>
-    </Flex>
+          Log In
+        </Button>
+        <Text align={'center'} fontWeight={'bold'}>
+          or
+        </Text>
+        <Button
+          leftIcon={<FontAwesomeIcon icon={faGithub} />}
+          type={'solid'}
+          onClick={() => {
+            signIn('github', { callbackUrl: callbackUrl });
+          }}
+        >
+          Sign In with Github
+        </Button>
+        <Button
+          leftIcon={<FontAwesomeIcon icon={faMicrosoft} />}
+          type={'solid'}
+          onClick={() => {
+            signIn('github', { callbackUrl: callbackUrl });
+          }}
+        >
+          Sign In with Microsoft
+        </Button>
+        <Button
+          leftIcon={<FontAwesomeIcon icon={faGoogle} />}
+          type={'solid'}
+          onClick={() => {
+            signIn('github', { callbackUrl: callbackUrl });
+          }}
+        >
+          Sign In with Google
+        </Button>
+      </Stack>
+    </>
   );
-}
+};
+
+export default SignIn;
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,11 +2,9 @@ import AuthProvider from '@/context/AuthProvider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import StyleProvider from '@/context/StyleProvider';
-// import Font Awesome CSS
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
 import { config } from '@fortawesome/fontawesome-svg-core';
-import { Box } from '@chakra-ui/react';
+
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
 config.autoAddCss = false;
