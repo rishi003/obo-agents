1ce6d62af661682fc12d77bfe069ad5ab1ec52aa
Tarun-Khilani <khilanitarun@gmail.com>
2023-08-12 19:22:38 +0530

    Added tests for vector store and doc loader

--- app/tests/test_vector_store.py
+++ app/tests/test_vector_store.py
@@ -30,7 +30,29 @@ def test_chroma():
     # ids = chroma_db.add_texts(docs)
     matching_docs = chroma_db.get_matching_text("A man is playing the guitar")
     print(matching_docs)
-    assert docs[0] in [doc.text_content for doc in matching_docs]
+    assert docs[0] in [doc.page_content for doc in matching_docs]
     matching_docs = chroma_db.get_matching_text("The sky is yellow")
     print(matching_docs)
-    assert docs[2] in [doc.text_content for doc in matching_docs]
+    assert docs[2] in [doc.page_content for doc in matching_docs]
+    
+def test_chroma_deletion():
+    embedding_model = HuggingfaceEmbedding()
+    chroma_db = ChromaDB(
+        collection_name="test_collection",
+        embedding_model=embedding_model,
+        text_field="test_field",
+    )
+    docs = [
+        "A man is playing the piano",
+        "A man is playing the violin",
+        "The sky is blue",
+        "The sun is shining"
+    ]
+    metadatas = [{"test_field": "test_id"} for _ in docs]
+    ids = chroma_db.add_texts(docs, metadatas)
+    emb_count = chroma_db.get_index_stats()
+    print(emb_count)
+    chroma_db.delete_embeddings_from_vector_db(where={"test_field": "test_id"})
+    emb_count = chroma_db.get_index_stats()
+    print(emb_count)
+    assert emb_count["embeddings_count"] == 0, "Embeddings are not deleted"
--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import os
 from app.tests.test_summarizer import test_summarize
-from app.tests.test_vector_store import test_hf_emb, test_chroma
+from app.tests.test_vector_store import test_hf_emb, test_chroma, test_chroma_deletion
+from app.tests.test_document_loader import test_doc_loader
 from app.utils.utils import get_root_input_dir
 
 if __name__ == "__main__":
@@ -10,4 +11,8 @@ if __name__ == "__main__":
 
     # test_hf_emb()
 
-    test_chroma()
+    # test_chroma()
+
+    test_chroma_deletion()
+
+    # test_doc_loader()
